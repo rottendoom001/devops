4062f26f2c7f2934afadf2b2ebf5c58647e2169b
Ubuntu <ubuntu@ip-172-31-61-1.ec2.internal>
2020-04-08 23:06:00 +0000

    sg.tf

---
+++ sg.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "bootcamp-devops"
+  description = "Allow SSH and HTTPinbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-sg"
+  }
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
